8dacab2a6d7599318f804f36b4652a4134912ec0
Victor_Komolafe <victorkomolafe123@gmail.com>
2024-04-02 12:25:53 +0100

    added some ui feature for post activity

--- src/nav.jsx
+++ src/nav.jsx
@@ -16,7 +16,7 @@ function Nav() {
 
 </div>
 {/* dashboard section the green part */}
-<div className="div-dash top-[120px] w-auto cursor-pointer hover:bg-[#36955c] sm:w-[206px] h-[45px] bg-[#0FA44A] text-white font-extralight flex md:flex-row p-2 
+<div className="div-dash top-[120px] w-auto cursor-pointer hover:bg-[#36955c] sm:w-[206px] h-[45px] bg-[#0FA44A] animate-pulse text-white font-extralight flex md:flex-row p-2 
 " >
 <div className=" flex md:flex-row  items-center justify-center  cursor-pointer hover:bg-[#36955c]">
   <div className="pr-2 pl-4">
@@ -57,7 +57,7 @@ function Nav() {
 </svg>
 </span> Settings</li>
 <div className='flex flex-row items-center justify-center  flex-end md:mt-28'>
-<h1 className='font-rubik font-normal text-[16px] cursor-pointer  md:flex flex-row  text-[#0FA44A]'> <span className='pr-2 '><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<h1 className='font-rubik font-normal text-[16px] cursor-pointer  md:flex flex-row  text-[#0FA44A]  hover:animate-pulse hover:text-[#f76c6c]'> <span className='pr-2 '><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M6 2H15C15.5304 2 16.0391 2.21071 16.4142 2.58579C16.7893 2.96086 17 3.46957 17 4V6H15V4H6V20H15V18H17V20C17 20.5304 16.7893 21.0391 16.4142 21.4142C16.0391 21.7893 15.5304 22 15 22H6C5.46957 22 4.96086 21.7893 4.58579 21.4142C4.21071 21.0391 4 20.5304 4 20V4C4 3.46957 4.21071 2.96086 4.58579 2.58579C4.96086 2.21071 5.46957 2 6 2Z" fill="#0FA44A"/>
 <path d="M16.09 15.59L17.5 17L22.5 12L17.5 7L16.09 8.41L18.67 11H9V13H18.67L16.09 15.59Z" fill="#0FA44A"/>
 </svg>
